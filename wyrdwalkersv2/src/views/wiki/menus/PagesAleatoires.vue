f7efbe8ab1209afbaf92ab177b92d837ec1d9a12
Enech <enech.project@gmail.com>
2019-12-28 11:19:36 +0100

    Pages aléatoires
    Ajout des images dans les résultats renvoyés

--- wyrdwalkersv2/src/views/wiki/menus/PagesAleatoires.vue
+++ wyrdwalkersv2/src/views/wiki/menus/PagesAleatoires.vue
@@ -1,21 +1,36 @@
 <template>
-    <div class="pa-3">
-        <v-card class="pa-3">
-            <v-card-title class="headline">Pages aléatoires</v-card-title>
-            <v-divider class="mb-3"></v-divider>
-            <v-row dense>
-                <v-col cols="12" md="4" v-for="(page,index) in randomPages" :key="index">
-                    <v-card class="pa-3" dark>
-                        <v-card-title>{{page.title.titleVF}}</v-card-title>
-                        <v-card-content class="caption font-weight-light">{{getRenderText(page)}}</v-card-content>
-                        <v-card-actions>
-                            <v-btn block dark color="red darken-4" :href="`/wiki/lore/${page.title.titleVF}`" target="_blank">Voir</v-btn>
-                        </v-card-actions>
-                    </v-card>
-                </v-col>
-            </v-row>
-        </v-card>
-    </div>
+  <div class="pa-3">
+    <v-card class="pa-3">
+      <v-card-title class="headline">Pages aléatoires</v-card-title>
+      <v-divider class="mb-3"></v-divider>
+      <v-row dense>
+        <v-col cols="12" md="4" v-for="(page,index) in randomPages" :key="index">
+          <v-card class="pa-3" dark>
+            <div class="d-flex flex-no-wrap justify-space-between">
+              <div>
+                <v-card-title class="headline" v-text="page.title.titleVF"></v-card-title>
+
+                <v-card-subtitle v-text="getRenderText(page)"></v-card-subtitle>
+              </div>
+
+              <v-avatar class="ma-3" size="125" tile v-if="page.content[0].picture != ''">
+                <v-img :src="page.content[0].picture"></v-img>
+              </v-avatar>
+            </div>
+            <v-card-actions>
+              <v-btn
+                block
+                dark
+                color="red darken-4"
+                :href="`/wiki/lore/${page.title.titleVF}`"
+                target="_blank"
+              >Voir</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-card>
+  </div>
 </template>
 
 <script lang="ts">
@@ -25,25 +40,28 @@ import WikiPage from "../../../model/WikiPage.model";
 
 export default Vue.extend({
   name: "RandomPages",
-  created: function(){
-      this.fetchRandomPages();
+  created: function() {
+    this.fetchRandomPages();
   },
   computed: {
-      randomPages: function(){
-          return store.getters.randomWikiPages;
-      }
+    randomPages: function() {
+      return store.getters.randomWikiPages;
+    }
   },
-  methods:{
-    getRenderText: function(page: WikiPage){
-        var text = page.generalInfos !== undefined ? page.generalInfos.vf : page.content[0].textVF;
-        var node = document.createElement("div");
-        node.innerHTML = text;
-        var innerText = node.innerText;
-        var result = innerText.substring(0,149) + '...';
-        return result;
+  methods: {
+    getRenderText: function(page: WikiPage) {
+      var text =
+        page.generalInfos !== undefined
+          ? page.generalInfos.vf
+          : page.content[0].textVF;
+      var node = document.createElement("div");
+      node.innerHTML = text;
+      var innerText = node.innerText;
+      var result = innerText.substring(0, 149) + "...";
+      return result;
     },
-    fetchRandomPages: function(){
-        store.dispatch("fetchRandomWikiPages");
+    fetchRandomPages: function() {
+      store.dispatch("fetchRandomWikiPages");
     }
   },
   data: () => ({})
