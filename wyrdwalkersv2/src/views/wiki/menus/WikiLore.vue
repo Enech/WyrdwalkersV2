e4fcdfff5342e9d4908cfe23490edf5be80b3a07
Enech <enech.project@gmail.com>
2019-12-31 03:21:32 +0100

    Metas
    Correction bug

--- wyrdwalkersv2/src/views/wiki/menus/WikiLore.vue
+++ wyrdwalkersv2/src/views/wiki/menus/WikiLore.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="pa-3">
-    <v-card class="pa-3">
+    <v-card class="pa-3" v-if="!loading">
       <v-card-title class="headline">{{page.title.titleVF}}</v-card-title>
       <v-tabs grow show-arrows>
         <v-tab v-if="page.generalInfos != undefined" href="#tab-general">Général</v-tab>
@@ -124,12 +124,21 @@
         </v-tab-item>
       </v-tabs>
     </v-card>
+    <v-dialog v-model="loading" hide-overlay persistent width="300">
+        <v-card color="primary" dark>
+          <v-card-text>
+            Chargement...
+            <v-progress-linear indeterminate color="white" class="mb-0"></v-progress-linear>
+          </v-card-text>
+        </v-card>
+      </v-dialog>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
 import store from "../../../store";
+import WikiPage from "../../../model/WikiPage.model";
 
 export default Vue.extend({
   name: "WikiLore",
@@ -138,7 +147,10 @@ export default Vue.extend({
   },
   methods: {
     fetchWikiPage: function(name: string) {
-      store.dispatch("fetchWikiPage", name);
+      store.dispatch("fetchWikiPage", name).then(() => {
+        this.page = store.getters.wikipage;
+        this.loading = false;
+      });
     },
     getEmbedUrl: function(url: string) {
       var urlTab = url.split("watch?v=");
@@ -198,33 +210,32 @@ export default Vue.extend({
       return result;
     }
   },
-  data: () => ({}),
-  computed: {
-    page: function() {
-      return store.getters.wikipage;
-    }
-  },
-  metaInfo() {
+  data: () => ({
+    page: new WikiPage(),
+    loading: true
+  }),
+  metaInfo: function() {
+    var tempPage = this.page;
     return {
-      title: `${this.$route.params.pagename}`,
+      title: tempPage ? `${tempPage.title.titleVF}` : "Wiki",
       meta: [
         {
           name: "description",
-          content: `Page codex de ${this.$route.params.pagename}`
+          content: tempPage ? `Page codex de ${tempPage.title.titleVF}` : "Page de codex"
         },
         {
           name: "keywords",
-          content: `philosophy,philosophie,mythologie,mythologie,codex,association,panthéons,fate,destin,dieux,titans,dragons,${this.$route.params.pagename}`
+          content: tempPage ? `philosophy,philosophie,mythologie,mythologie,codex,association,panthéons,fate,destin,dieux,titans,dragons,${tempPage.title.titleVF}` : "philosophy,philosophie,mythologie,mythologie,codex,association,panthéons,fate,destin,dieux,titans,dragons"
         },
         { name: "author", content: "Thomas Gely" },
         {
           property: "og:url",
-          content: `https://wyrdwalkers.com/wiki/lore/${this.$route.params.pagename}`
+          content: tempPage ? `https://wyrdwalkers.com/wiki/lore/${tempPage.title.titleVF}` : ""
         },
         { property: "og:type", content: "website" },
         {
           property: "og:description",
-          content: `Page codex de ${this.$route.params.pagename}`
+          content: tempPage ? `Page codex de ${tempPage.title.titleVF}` : "Page de codex"
         },
         { property: "og:image", content: "https://i.imgur.com/xSW0VL0.png" }
       ]
