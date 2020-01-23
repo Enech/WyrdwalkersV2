df2a950f5707aeed7ae1a424b88125ca460b1938
Thomas Gely <thomas.gely@external.engie.com>
2020-01-23 11:43:08 +0100

    REtrait composant WikiMeta

--- wyrdwalkersv2/src/views/wiki/menus/WikiLore.vue
+++ wyrdwalkersv2/src/views/wiki/menus/WikiLore.vue
@@ -3,7 +3,10 @@
     <v-card class="pa-3 lore" v-if="!loading" id="app-content-custom">
       <v-card-title class="headline">{{page.title.titleVF}}</v-card-title>
       <v-tabs grow show-arrows>
-        <v-tab v-if="page.generalInfos != undefined" href="#tab-general">{{$t("wiki.contents.lore.general")}}</v-tab>
+        <v-tab
+          v-if="page.generalInfos != undefined"
+          href="#tab-general"
+        >{{$t("wiki.contents.lore.general")}}</v-tab>
         <v-tab v-if="page.myth != undefined" href="#tab-myth">{{$t("wiki.contents.lore.myth")}}</v-tab>
         <v-tab
           v-for="(content,index) in page.content"
@@ -21,9 +24,32 @@
               </v-col>
             </v-row>
             <v-row dense>
-              <v-col cols="12" md="9" v-html="$i18n.locale == 'fr' ? page.generalInfos.vf : page.generalInfos.vo"></v-col>
+              <v-col
+                cols="12"
+                md="9"
+                v-html="$i18n.locale == 'fr' ? page.generalInfos.vf : page.generalInfos.vo"
+              ></v-col>
               <v-col cols="12" md="3">
-                <wiki-meta :picture="page.content[0].picture" :pictureAuthor="page.content[0].picAuthor" :music="page.content[0].music"/>
+                <v-img :src="page.content[0].picture" eager></v-img>
+                <v-list-item class="blue mb-1" dark v-if="page.content[0].picture != ''">
+                  <v-list-item-icon>
+                    <v-icon small>edit</v-icon>
+                  </v-list-item-icon>
+                  <v-list-item-content>
+                    <span v-if="page.content[0].picAuthor != ''">{{page.content[0].picAuthor}}</span>
+                    <span v-else>{{$t("wiki.contents.lore.noAuthor")}}</span>
+                  </v-list-item-content>
+                </v-list-item>
+                <div v-if="music != ''">
+                  <iframe
+                    width="100%"
+                    height="100"
+                    :src="page.content[0].music"
+                    frameborder="0"
+                    allow="autoplay; encrypted-media"
+                    allowfullscreen
+                  ></iframe>
+                </div>
               </v-col>
             </v-row>
           </v-card>
@@ -41,7 +67,26 @@
             <v-row dense>
               <v-col cols="12" md="9" v-html="$i18n.locale == 'fr' ? page.myth.vf : page.myth.vo"></v-col>
               <v-col cols="12" md="3">
-                <wiki-meta :picture="page.content[0].picture" :pictureAuthor="page.content[0].picAuthor" :music="page.content[0].music"/>
+                <v-img :src="page.content[0].picture" eager></v-img>
+                <v-list-item class="blue mb-1" dark v-if="page.content[0].picture != ''">
+                  <v-list-item-icon>
+                    <v-icon small>edit</v-icon>
+                  </v-list-item-icon>
+                  <v-list-item-content>
+                    <span v-if="page.content[0].picAuthor != ''">{{page.content[0].picAuthor}}</span>
+                    <span v-else>{{$t("wiki.contents.lore.noAuthor")}}</span>
+                  </v-list-item-content>
+                </v-list-item>
+                <div v-if="music != ''">
+                  <iframe
+                    width="100%"
+                    height="100"
+                    :src="page.content[0].music"
+                    frameborder="0"
+                    allow="autoplay; encrypted-media"
+                    allowfullscreen
+                  ></iframe>
+                </div>
               </v-col>
             </v-row>
           </v-card>
@@ -58,9 +103,32 @@
               </v-col>
             </v-row>
             <v-row dense>
-              <v-col cols="12" md="9" v-html="$i18n.locale == 'fr' ? content.textVF : content.textVO"></v-col>
+              <v-col
+                cols="12"
+                md="9"
+                v-html="$i18n.locale == 'fr' ? content.textVF : content.textVO"
+              ></v-col>
               <v-col cols="12" md="3">
-                <wiki-meta :picture="content.picture" :pictureAuthor="content.picAuthor" :music="content.music"/>
+                <v-img :src="content.picture" eager></v-img>
+                <v-list-item class="blue mb-1" dark v-if="content.picture != ''">
+                  <v-list-item-icon>
+                    <v-icon small>edit</v-icon>
+                  </v-list-item-icon>
+                  <v-list-item-content>
+                    <span v-if="content.picAuthor != ''">{{content.picAuthor}}</span>
+                    <span v-else>{{$t("wiki.contents.lore.noAuthor")}}</span>
+                  </v-list-item-content>
+                </v-list-item>
+                <div v-if="music != ''">
+                  <iframe
+                    width="100%"
+                    height="100"
+                    :src="content.music"
+                    frameborder="0"
+                    allow="autoplay; encrypted-media"
+                    allowfullscreen
+                  ></iframe>
+                </div>
               </v-col>
             </v-row>
           </v-card>
@@ -82,13 +150,9 @@
 import Vue from "vue";
 import store from "../../../store";
 import WikiPage from "../../../model/WikiPage.model";
-import WikiMeta from "../../../components/wiki/WikiMeta.vue"
 
 export default Vue.extend({
   name: "WikiLore",
-  components: {
-    "wiki-meta": WikiMeta
-  },
   created: function() {
     this.fetchWikiPage(this.$route.params.pagename);
   },
@@ -96,11 +160,17 @@ export default Vue.extend({
     document.dispatchEvent(new Event("custom-post-render-event"));
   },
   computed: {
-    generalDefined: function(){
-      return this.page.generalInfos != undefined && (this.page.generalInfos.vf !== '' || this.page.generalInfos.vo !== '')
+    generalDefined: function() {
+      return (
+        this.page.generalInfos != undefined &&
+        (this.page.generalInfos.vf !== "" || this.page.generalInfos.vo !== "")
+      );
     },
-    mythDefined: function(){
-      return this.page.myth != undefined && (this.page.myth.vf !== '' || this.page.myth.vo !== '')
+    mythDefined: function() {
+      return (
+        this.page.myth != undefined &&
+        (this.page.myth.vf !== "" || this.page.myth.vo !== "")
+      );
     }
   },
   methods: {
@@ -124,7 +194,7 @@ export default Vue.extend({
         disabled: true
       };
       result.push({
-        text: this.$i18n.locale == 'fr' ? "Accueil" : "Home",
+        text: this.$i18n.locale == "fr" ? "Accueil" : "Home",
         href: "/wiki/home"
       });
       // Le cas "empty" induit de ne pas mettre le breadcrumb correspondant danas la liste
@@ -132,25 +202,31 @@ export default Vue.extend({
         switch (content.faction) {
           case "dieux":
             result.push({
-              text: this.$i18n.locale == 'fr' ? "Panthéons" : "Pantheons",
+              text: this.$i18n.locale == "fr" ? "Panthéons" : "Pantheons",
               href: "/wiki/lore/panthéons"
             });
             break;
           case "society":
             result.push({
-              text: this.$i18n.locale == 'fr' ? "Sociétés Secrètes" : "Secret Societies",
+              text:
+                this.$i18n.locale == "fr"
+                  ? "Sociétés Secrètes"
+                  : "Secret Societies",
               href: "/wiki/lore/sociétés-secrètes"
             });
             break;
           case "otherworld":
             result.push({
-              text: this.$i18n.locale == 'fr' ? "Outres-Mondes" : "Otherworlds",
+              text: this.$i18n.locale == "fr" ? "Outres-Mondes" : "Otherworlds",
               href: "/wiki/lore/outres-mondes"
             });
             break;
           case "titans":
             result.push({
-              text: this.$i18n.locale == 'fr' ? "Royaumes Titaniques" : "Titanrealms",
+              text:
+                this.$i18n.locale == "fr"
+                  ? "Royaumes Titaniques"
+                  : "Titanrealms",
               href: "/wiki/lore/titans"
             });
             break;
