4be21a0930ff9f05dbd2051886cc590d34836349
Thomas Gely <thomas.gely@external.engie.com>
2020-01-24 11:30:19 +0100

    Create Approche.model.ts

---
+++ wyrdwalkersv2/src/model/explorer/Approche.model.ts
@@ -0,0 +1,15 @@
+﻿export default class Approche {
+    public nameVF: string;
+    public nameVO: string;
+    public logos: number;
+    public kosmos: number;
+    public eros: string;
+
+    constructor() {
+        this.nameVF = '';
+        this.nameVO = '';
+        this.logos = -1;
+        this.kosmos = -1;
+        this.eros = -1;
+    }    
+}
