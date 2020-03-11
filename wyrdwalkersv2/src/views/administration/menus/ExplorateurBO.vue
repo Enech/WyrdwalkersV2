729888a51119e66a49fed26bf1208ce179b263f0
Enech <enech.project@gmail.com>
2020-03-11 19:49:29 +0100

    Update ExplorateurBO.vue

--- wyrdwalkersv2/src/views/administration/menus/ExplorateurBO.vue
+++ wyrdwalkersv2/src/views/administration/menus/ExplorateurBO.vue
@@ -42,10 +42,24 @@
                 </v-row>
                 <v-row>
                   <v-col cols="12" sm="4">
-                    <v-select label="Faction" :items="factions" return-object item-text="name" item-value="key" v-model="editedItem.faction"></v-select>
+                    <v-select
+                      label="Faction"
+                      :items="factions"
+                      return-object
+                      item-text="name"
+                      item-value="key"
+                      v-model="editedItem.faction"
+                    ></v-select>
                   </v-col>
                   <v-col cols="12" sm="4">
-                    <v-select label="Origine" :items="origins" v-model="editedItem.origin" item-value="_id" item-text="nameVF"></v-select>
+                    <v-select
+                      label="Origine"
+                      :items="origins"
+                      v-model="editedItem.origin"
+                      item-value="_id"
+                      item-text="nameVF"
+                      return-object
+                    ></v-select>
                   </v-col>
                   <v-col cols="12" sm="4">
                     <v-select
@@ -79,16 +93,17 @@
                   </v-col>
                 </v-row>
                 <v-row>
-                    <v-col cols="12" sm="4">
-                        <v-text-field label="Logos" v-model="editedItem.logos"></v-text-field>
-                    </v-col>
-                    <v-col cols="12" sm="4">
-                        <v-text-field label="Kosmos" v-model="editedItem.kosmos"></v-text-field>
-                    </v-col>
-                    <v-col cols="12" sm="4">
-                        <v-text-field label="Eros" v-model="editedItem.eros"></v-text-field>
-                    </v-col>
-                  </v-row>
+                  <v-col cols="12">Pour le calcul automatique du LEK, laisser les scores à -1</v-col>
+                  <v-col cols="12" sm="4">
+                    <v-text-field label="Logos" v-model="editedItem.logos"></v-text-field>
+                  </v-col>
+                  <v-col cols="12" sm="4">
+                    <v-text-field label="Kosmos" v-model="editedItem.kosmos"></v-text-field>
+                  </v-col>
+                  <v-col cols="12" sm="4">
+                    <v-text-field label="Eros" v-model="editedItem.eros"></v-text-field>
+                  </v-col>
+                </v-row>
               </v-container>
             </v-card-text>
             <v-card-actions>
@@ -174,6 +189,13 @@ export default Vue.extend({
     },
     approaches: function() {
       return store.getters.approaches;
+    },
+    needAutoLEKCompute: function() {
+      return (
+        this.editedItem.logos < 0 &&
+        this.editedItem.kosmos < 0 &&
+        this.editedItem.eros < 0
+      );
     }
   },
   methods: {
@@ -184,12 +206,18 @@ export default Vue.extend({
       });
     },
     addExplorerEntity: function() {
+      if (this.needAutoLEKCompute) {
+        this.computeLEKScores();
+      }
       store.dispatch("addExplorerEntity", this.editedItem).then(() => {
         this.closeDialog();
         Object.assign(this.editedItem, new EntityExplorer());
       });
     },
     sendUpdate: function() {
+      if (this.needAutoLEKCompute) {
+        this.computeLEKScores();
+      }
       store.dispatch("updateExplorerEntity", this.editedItem).then(() => {
         this.closeDialog();
         Object.assign(this.editedItem, new EntityExplorer());
@@ -210,6 +238,123 @@ export default Vue.extend({
       Object.assign(this.editedItem, new EntityExplorer());
       this.dialog = false;
       this.fetchExplorerEntities();
+    },
+    computeLEKScores: function() {
+      var approcheObj = {
+        logos: 0,
+        kosmos: 0,
+        eros: 0
+      };
+      var domainObj = {
+        logos: 0,
+        kosmos: 0,
+        eros: 0
+      };
+      var personalityObj = {
+        logos: 0,
+        kosmos: 0,
+        eros: 0
+      };
+      var originObj = {
+        logos: 0,
+        kosmos: 0,
+        eros: 0
+      };
+
+      if (approcheObj) {
+        for (var i = 0; i < this.editedItem.approaches.length; i++) {
+          var approcheID = this.editedItem.approaches[i];
+          var approche = this.approaches.filter((obj: any) => {
+            return obj._id === approcheID;
+          })[0];
+          approcheObj.logos += approche.logos / (i + 1);
+          approcheObj.kosmos += approche.kosmos / (i + 1);
+          approcheObj.eros += approche.eros / (i + 1);
+        }
+      }
+      if (domainObj) {
+        for (i = 0; i < this.editedItem.domains.length; i++) {
+          var domaineID = this.editedItem.domains[i];
+          var domaine = this.domains.filter((obj: any) => {
+            return obj._id === domaineID;
+          })[0];
+          domainObj.logos += domaine.logos / this.editedItem.domains.length;
+          domainObj.kosmos += domaine.kosmos / this.editedItem.domains.length;
+          domainObj.eros += domaine.eros / this.editedItem.domains.length;
+        }
+      }
+      if (personalityObj) {
+        for (i = 0; i < this.editedItem.personalities.length; i++) {
+          var personalityID = this.editedItem.personalities[i];
+          var personality = this.personalities.filter((obj: any) => {
+            return obj._id === personalityID;
+          })[0];
+          personalityObj.logos +=
+            personality.logos / this.editedItem.personalities.length;
+          personalityObj.kosmos +=
+            personality.kosmos / this.editedItem.personalities.length;
+          personalityObj.eros +=
+            personality.eros / this.editedItem.personalities.length;
+        }
+      }
+      if (originObj) {
+        originObj.logos = this.editedItem.origin.logos;
+        originObj.kosmos = this.editedItem.origin.kosmos;
+        originObj.eros = this.editedItem.origin.eros;
+      }
+      if (approcheObj && domainObj && personalityObj && originObj) {
+        if (this.editedItem.faction.key == ExplorerFaction.TITAN) {
+          this.editedItem.logos =
+            Math.round(
+              (0.1 * approcheObj.logos +
+                0.25 * domainObj.logos +
+                0.25 * personalityObj.logos +
+                0.4 * originObj.logos) *
+                10
+            ) / 10;
+          this.editedItem.kosmos =
+            Math.round(
+              (0.1 * approcheObj.kosmos +
+                0.25 * domainObj.kosmos +
+                0.25 * personalityObj.kosmos +
+                0.4 * originObj.kosmos) *
+                10
+            ) / 10;
+          this.editedItem.eros =
+            Math.round(
+              (0.1 * approcheObj.eros +
+                0.25 * domainObj.eros +
+                0.25 * personalityObj.eros +
+                0.4 * originObj.eros) *
+                10
+            ) / 10;
+        } else {
+          this.editedItem.logos =
+            Math.round(
+              (0.15 * approcheObj.logos +
+                0.35 * domainObj.logos +
+                0.35 * personalityObj.logos +
+                0.15 * originObj.logos) *
+                10
+            ) / 10;
+          this.editedItem.kosmos =
+            Math.round(
+              (0.15 * approcheObj.kosmos +
+                0.35 * domainObj.kosmos +
+                0.35 * personalityObj.kosmos +
+                0.15 * originObj.kosmos) *
+                10
+            ) / 10;
+          this.editedItem.eros =
+            Math.round(
+              (0.15 * approcheObj.eros +
+                0.35 * domainObj.eros +
+                0.35 * personalityObj.eros +
+                0.15 * originObj.eros) *
+                10
+            ) / 10;
+        }
+      }
     }
   },
   data: () => ({
